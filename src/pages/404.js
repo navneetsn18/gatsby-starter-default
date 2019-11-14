34ded03b368503a529fb4b7971d28941130ed3ff
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-11-15 02:06:43 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,20 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+	<div class="toggle"></div>
+        <div class="content">
+           <Layout>
+		    <SEO title="404: Not found" />
+		    <h1>NOT FOUND</h1>
+		    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		  </Layout>
+    </div>
+    $(document).ready(function(){
+        $('.toggle').click(function(){
+            $('.toggle').toggleClass('active')
+            $('body').toggleClass('night')
+        })
+    })
 )
 
 export default NotFoundPage
