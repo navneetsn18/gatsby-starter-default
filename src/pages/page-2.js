597900c80c23c60f46961668e29ad53a686e4764
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-11-15 02:07:30 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,12 +5,21 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+	<div class="toggle"></div>
+	        <div class="content">
+	           <Layout>
+	    <SEO title="Page two" />
+	    <h1>Hi from the second page</h1>
+	    <p>Welcome to page 2</p>
+	    <Link to="/">Go back to the homepage</Link>
+	  </Layout>
+    </div>
+    $(document).ready(function(){
+        $('.toggle').click(function(){
+            $('.toggle').toggleClass('active')
+            $('body').toggleClass('night')
+        })
+    })
 )
 
 export default SecondPage
