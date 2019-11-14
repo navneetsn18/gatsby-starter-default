af69ba94a38b3290dbdeffda214e30e4d3c26b26
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-11-15 02:07:08 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,28 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div class="toggle"></div>
+        <div class="content">
+          <Layout>
+            <SEO title="Home" />
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+              <Image />
+            </div>
+            <Link to="/page-2/">Go to page 2</Link>
+          </Layout>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+//<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    $(document).ready(function(){
+        $('.toggle').click(function(){
+            $('.toggle').toggleClass('active')
+            $('body').toggleClass('night')
+        })
+    })
+
 )
 
 export default IndexPage
